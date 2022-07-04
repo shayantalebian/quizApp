b37d3b037623af2d1143b7caa235f2a74a0451ab
Shayan Talebian <shayantalebianwork@gmail.com>
2022-07-04 20:22:06 +0430

    Create correct and incorrect classes for answer

--- game.js
+++ game.js
@@ -7,34 +7,32 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-  {
-    question: "Inside which HTML element do we put the JavaScript?",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1,
-  },
+let questions = [{
+        question: "Inside which HTML element do we put the JavaScript?",
+        choice1: "<script>",
+        choice2: "<javascript>",
+        choice3: "<js>",
+        choice4: "<scripting>",
+        answer: 1,
+    },
 
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js>",
-    choice3: "<script src='xxx.js>",
-    choice4: "<script file='xxx.js>",
-    answer: 3,
-  },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        choice1: "<script href='xxx.js'>",
+        choice2: "<script name='xxx.js>",
+        choice3: "<script src='xxx.js>",
+        choice4: "<script file='xxx.js>",
+        answer: 3,
+    },
 
-  {
-    question: "How do you write 'Hello World!' in an alert box in js ?",
-    choice1: "msgBox('Hello world!')",
-    choice2: "alertbox('Hello world!')",
-    choice3: "msg('Hello world!')",
-    choice4: "alert('Hello world!')",
-    answer: 4,
-  },
+    {
+        question: "How do you write 'Hello World!' in an alert box in js ?",
+        choice1: "msgBox('Hello world!')",
+        choice2: "alertbox('Hello world!')",
+        choice3: "msg('Hello world!')",
+        choice4: "alert('Hello world!')",
+        answer: 4,
+    },
 ];
 
 //CONSTANTS
@@ -42,44 +40,60 @@ const CORRECT_BONUS = 10;
 const MAX_QUESTION = 3;
 
 startGame = () => {
-  questionCounter = 0;
-  score = 0;
-  availableQuestions = [...questions];
-  console.log(availableQuestions);
-  getNewQuestions();
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestions();
 };
 
 getNewQuestions = () => {
-  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTION) {
-    return window.location.assign("/end.html");
-  }
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTION) {
+        return window.location.assign("/end.html");
+    }
 
-  questionCounter++;
-  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-  currentQuestion = availableQuestions[questionIndex];
-  console.log(questionIndex);
-  console.log(currentQuestion);
-  question.innerHTML = currentQuestion.question;
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    console.log(questionIndex);
+    console.log(currentQuestion);
+    question.innerHTML = currentQuestion.question;
 
-  choices.forEach((choice) => {
-    const number = choice.dataset["number"];
-    choice.innerText = currentQuestion["choice" + number];
-  });
+    choices.forEach((choice) => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
 
-  availableQuestions.splice(questionIndex, 1);
-  acceptingAnswer = true;
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswer = true;
 };
 
 choices.forEach((choice) => {
-  choice.addEventListener("click", (e) => {
-    if (!acceptingAnswer) return;
+    choice.addEventListener("click", (e) => {
+        if (!acceptingAnswer) return;
 
-    acceptingAnswer = false;
-    const selectedChoice = e.target;
-    const seleectedAnswer = selectedChoice.dataset["number"];
-    console.log(seleectedAnswer);
-    getNewQuestions();
-  });
+        acceptingAnswer = false;
+        const selectedChoice = e.target;
+        const seleectedAnswer = selectedChoice.dataset["number"];
+
+        let calssToApply = "incorrect";
+        if (seleectedAnswer == currentQuestion.answer) {
+            calssToApply = "correct";
+        }
+
+        console.log(calssToApply);
+
+        selectedChoice.parentElement.classList.add(calssToApply);
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(calssToApply);
+            getNewQuestions();
+        }, 1000);
+
+        // Ternery op
+        // const calssToApply = seleectedAnswer == currentQuestion.answer ? "correct" : "incorrect"
+
+        console.log(seleectedAnswer == currentQuestion.answer);
+    });
 });
 
 startGame();
